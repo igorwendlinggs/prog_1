0dd11c87848113ea91ff38b1439514ca3a85f613
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2022-11-20 23:39:17 -0300

    Update and rename Ex. 1 to 01.c

---
+++ Exercícios/Lista 1: Estruturas Condicionais/01.c
@@ -0,0 +1,4 @@
+//Crie um programa em linguagem C que imprima o nome na tela o nome de quem escreveu o programa.
+#include <stdio.h>
+
+int main(void){printf("Igor Wendling Gurgel Silva\n"); return 0;}
