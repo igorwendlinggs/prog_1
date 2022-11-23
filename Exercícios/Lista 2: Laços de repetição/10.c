39f6959b59ed08757430bacc4394809cf7723e69
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2022-11-23 11:02:28 -0300

    Create 10.c

---
+++ Exercícios/Lista 2: Laços de repetição/10.c
@@ -0,0 +1,15 @@
+//Crie um programa em linguagem C, utilizando o comando for, que imprima uma mensagem pedindo para o usuário digitar um número inteiro. O programa deve imprimir todos os números inteiros de 1 até o número informado pelo usuário, quando terminar de imprimir os números o programa deve terminar.
+
+#include <stdio.h>
+int main(void){
+  int i,x;
+printf("Digite 1 número inteiro:\n");
+scanf("%d", &x);
+for(i=1;i<=x;i++){
+  printf("%d\t", i);
+  }
+  
+
+  
+  return 0;
+}
