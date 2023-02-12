aedc66697c669c1613f17656aabda50572fbdd9a
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-12 11:15:40 -0300

    Update 01.c

--- Exercícios/Lista 5: Funções/01.c
+++ Exercícios/Lista 5: Funções/01.c
@@ -1,1 +1,29 @@
+//Termine de escrever a função areaRetangulo. Com a função corretamente terminada, o programa em linguagem C deverá receber dois valores reais que representem a base e a altura de um retângulo. A seguir deve utilizar a função areaRetangulo para calcular a área do retângulo e ao final imprimir na tela o valor calculado da área deste retângulo.
+#include <stdio.h>
+#include <stdlib.h>
+
+float areaRetangulo(float b, float h){ 
+  float x;
+  x = b*h;
+return x;
+
+}
+
+
+int main(){
+
+  float a, b, r;
+
+  printf("Digite o tamanho da base do retangulo: ");
+  scanf("%f", &a);
+
+  printf("Digite a altura do retangulo: ");
+  scanf("%f", &b);
+
+  
+  r = areaRetangulo(a, b);
+  printf("A area do retangulo eh %f\n", r);
+
+  return 0;
+  }
 
