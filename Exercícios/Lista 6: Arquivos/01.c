ce726b85845b903d970943bc932ed2c504b115af
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-12 17:07:25 -0300

    Update 01.c

--- Exercícios/Lista 6: Arquivos/01.c
+++ Exercícios/Lista 6: Arquivos/01.c
@@ -1,1 +1,17 @@
+/*Crie um programa em linguagem C que leia o arquivo palavra.dat, contendo uma única string, e imprima
+na tela a palavra contida neste arquivo.*/
+#include <stdio.h>
 
+int main(void){
+int i;
+char v[12];
+FILE *arq;
+
+arq= fopen("palavra.dat","r");
+for(i=0;i<12;i++)
+fscanf(arq,"%s",v);
+  /*fscanf(arq,"%c",&v[12];
+  fclose(arq);*/
+printf("%s",v);
+  
+return 0; }
