c8efac27367c361fc840c64046e69ea698b23713
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 09:52:55 -0300

    Create 02.c

---
+++ Exercícios/Lista 4: Struct/02.c
@@ -0,0 +1,7 @@
+/*Crie um trecho de código em linguagem C que defina uma estrutura (struct) chamada tLatlong que
+armazene: latitude (real), longitude (real) */
+
+struct tLatlong{
+float latitude;
+float longitude;
+};
