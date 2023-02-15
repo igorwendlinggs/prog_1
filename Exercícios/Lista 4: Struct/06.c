90a6ea86c128981fc695cf98b0d2453e0e368f7d
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 10:54:14 -0300

    Create 06.c

---
+++ Exercícios/Lista 4: Struct/06.c
@@ -0,0 +1,28 @@
+/*Crie um programa em linguagem C que receba 3 valores reais e os armazene na estrutura tVetorR3
+criada anteriormente.*/
+#include <stdio.h>
+
+typedef struct tVetorR3{
+float x;
+float y;
+float z;
+}vet;
+int main(void){
+vet a;
+
+printf("Insira x:\n");
+scanf("%f",&a.x);
+
+printf("Insira y:\n");
+scanf("%f",&a.y);
+
+printf("Insira z:\n");
+scanf("%f",&a.z);
+
+
+
+
+
+  
+
+}
