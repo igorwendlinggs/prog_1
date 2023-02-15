bf4ac5c772b94ab9585fe58493d877277b0e31d9
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 18:29:12 -0300

    Update 03.c

--- Exercícios/Lista 5: Funções/03.c
+++ Exercícios/Lista 5: Funções/03.c
@@ -1,1 +1,44 @@
+/*Termine de escrever a função crescDesc. Com a função corretamente terminada, o programa em
+linguagem C deverá receber cinco valores inteiros e armazená-los em um vetor de cinco posições. A
+seguir o programa deve percorrer o vetor e passar para a função crescDesc todos os pares de elementos
+sucesivos do vetor. Para cada par de valores recebidos, a função deve avaliar os dois valores e imprimir
+na tela se os valores estão em ordem crescente ou decrescente.*/
+#include <stdio.h>
+#include <stdlib.h>
 
+#define N 5
+
+
+void crescDesc(int a, int b){
+if(a>b){
+  printf("%d crescente %d\n", a, b); 
+}else{  printf("%d decrescente %d\n", b, a);
+  }
+
+
+}
+
+
+
+int main(){
+
+  int i, vet[N];
+
+
+
+  printf("Digite %d numeros inteiros\n", N);
+
+
+
+  for(i = 0; i < N; i++) 
+    scanf("%d", &vet[i]);
+
+  for(i = 0; i < N-1; i++)
+
+    crescDesc(vet[i], vet[i+1]);
+
+
+
+  return 0;
+
+  }
