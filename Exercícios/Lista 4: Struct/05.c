47b00d4678b65f2e570f4496c09adea329ec8185
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 10:05:39 -0300

    Update 05.c

--- Exercícios/Lista 4: Struct/05.c
+++ Exercícios/Lista 4: Struct/05.c
@@ -1,1 +1,24 @@
+/*Crie um programa em linguagem C que receba 2 valores reais e os armazene na estrutura tLatLong
+criada anteriormente.*/
+#include <stdio.h>
 
+typedef struct tLatlong{
+float lat;
+float longi;
+}latlong;
+int main(void){
+latlong x;
+
+printf("Insira a latitude:\n");
+scanf("%f",&x.lat);
+
+printf("Insira a longitude:\n");
+scanf("%f",&x.longi);
+
+
+
+printf("Latitude: %f\n", x.lat);
+  printf("Minuto: %f\n", x.longi);
+  
+
+}
