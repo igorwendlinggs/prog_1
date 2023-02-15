2185f439790686c2e67c4775219105689ba0024b
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 09:50:08 -0300

    Update and rename Exercícios/Lista 4: Struct (Não Feito)/01.c to Exercícios/Lista 4: Struct/01.c

---
+++ Exercícios/Lista 4: Struct/01.c
@@ -0,0 +1,8 @@
+/*Crie um trecho de código em linguagem C que defina uma estrutura (struct) chamada tRelogio que
+armazene: hora (inteiro), minuto (inteiro) e segundo (inteiro).*/
+
+struct tRelogio{
+int hora;
+int minuto;
+int segundo;
+};
