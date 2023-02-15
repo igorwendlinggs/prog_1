36ce6bccb2d694c9251b9e6c2cc63547d61f6525
Igor Wendling <110495757+igorwendlinggs@users.noreply.github.com>
2023-02-15 09:55:01 -0300

    Create 03.c

---
+++ Exercícios/Lista 4: Struct/03.c
@@ -0,0 +1,8 @@
+/*Crie um trecho de código em linguagem C que defina uma estrutura (struct) chamada tVetorR3 que
+armazene: x (real), y (real), z (real) */
+
+struct tVetorR3{
+float x;
+float y;
+float z;
+};
